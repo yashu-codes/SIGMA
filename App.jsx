64771a4d1ead386727af53a9feaabaac7fbd606a
YASWANTHI <yaswanthigollavilli@gmail.com>
2026-09-13 20:40:35 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import sigmaLogin from "./sigma-login.png";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8001";
+const API_URL = "https://sigma-mineguard-backend.onrender.com";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
